3278b6772163aab10bbd568c03bd121856db6956
dididiri1 <dididiri1@naver.com>
2024-03-29 10:59:02 +0900

    chore: AWS Secrets Manager 적용   - s3, rds 정보 암호화

--- src/main/java/sample/instagram/InstagramApplication.java
+++ src/main/java/sample/instagram/InstagramApplication.java
@@ -1,8 +1,11 @@
 package sample.instagram;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class InstagramApplication {
